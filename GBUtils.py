c6c74f7a7cdbba0d5ac5b3d9f7ae3d17ee86b4c4
GabrieleBattaglia <iz4apu@libero.it>
2024-10-29 03:31:41 +0100

    modified:   GBUtils.py New sonify 5 with sounddevices instead of pyo

--- GBUtils.py
+++ GBUtils.py
@@ -3,7 +3,7 @@
 	Data concepimento: lunedì 3 febbraio 2020.
 	Raccoglitore di utilità per i miei programmi.
 	Spostamento su github in data 27/6/2024. Da usare come submodule per gli altri progetti.
-	V14 di venerdì 4 ottobre 2024
+	V15 di martedì 29 ottobre 2024
 
 Lista utilità contenute in questo pacchetto
 	base62 3.0 di martedì 15 novembre 2022
@@ -15,8 +15,7 @@ Lista utilità contenute in questo pacchetto
 	menu V1.2.1 del 17 luglio 2024
 	percent V1.0 thu 28, september 2023
 	Scadenza 1.0 del 15/12/2021
-	sonify V2.5, april 27th, 2023
-	sonify2 a serie of data, V4.0, march 19th, 2024
+	sonify V5.0, october 29th, 2024
 	Vecchiume 1.0 del 15/12/2018
 '''
 
@@ -281,114 +280,70 @@ def gridapu(x=0.0, y=0.0, num=10):
 		qthloc += L[yn[8]] + L[yn[9]]
 	return qthloc
 
-def sonify(data=[], totalduration=10):
-	# Sonify a serie of data, V2.5, april 27th, 2023
-	# data= serie of value to sonify
-	# totalduration= duration of whole sonification in seconds
-	# it plays sound and returns nothing
-	datalength = len(data)
-	if datalength < 2:
-		print("Data serie too short.")
+def sonify(data_list, duration, ptm=False, vol=0.5):
+	'''
+	sonify V5.0, oct 29th, 2024
+	It sonify a serie of data float
+	RX a float list in between 5 and 500k values
+	   duration in ms
+				bool portamento, if it's true a glissando will be applied in between value transition
+	It returns nothing but audio
+	'''
+	import random
+	import numpy as np
+	import sounddevice as sd
+	if len(data_list) < 5 or len(data_list) > 500000:
+		print("Sonify4: data serie length out of range")
 		return
-	from time import sleep as S
-	from pyo import Server, SigTo, Sine, Pan
-	s=Server(verbosity=1).boot()
-	s.start()
-	singlesoundduration=totalduration/datalength
-	if singlesoundduration < 0.08:
-		totalduration = 0.08*datalength
-		singlesoundduration=totalduration/datalength
-		print(f"Duration too short, it has been set to {totalduration:.3f}.")
-	ramp=singlesoundduration/100*50
-	soundlowerlimit = 130 # about C3
-	soundupperlimit = 4186 # about C8
-	soundrange=soundupperlimit-soundlowerlimit
-	dataloweredge = 99999999999999
-	dataupperedge = -99999999999999
-	for j in data:
-		if j < dataloweredge: dataloweredge = j
-		if j > dataupperedge: dataupperedge = j
-	if dataloweredge>dataupperedge or dataupperedge<dataloweredge:
-		print("Wrong data serie.")
-		return
-	datarange = dataupperedge-dataloweredge
-	g=SigTo(dataloweredge+datarange/2, time=ramp)
-	a=Sine(freq=g, mul=.2)
-	p=Pan(a, pan=0).out()
-	salto=1/datalength
-	cnt=1
-	for j in data:
-		x = (j-dataloweredge) * 100/datarange
-		y = x/100*soundrange
-		p.pan=salto*cnt-salto/2
-		g.value=soundlowerlimit+y
-		S(singlesoundduration)
-		cnt+=1
-	s.stop()
-	s.shutdown()
+	try:
+		data_list = [float(value) for value in data_list]
+	except ValueError:
+		return  # Non produce alcun output se la conversione fallisce
+	if not (0.1 <= vol <= 1.0):
+		vol = max(0.1, min(vol, 1.0))
+	data_min = min(data_list)
+	data_max = max(data_list)
+	data_mean = sum(data_list) / len(data_list)
+	freq_min = 65.41   # C2
+	freq_max = 4186.01 # C8
+	if data_max - data_min == 0:
+		frequencies = [(freq_min + freq_max) / 2] * len(data_list)
+	else:
+		frequencies = [freq_min + (value - data_min) * (freq_max - freq_min) / (data_max - data_min) for value in data_list]
+	total_duration_sec = duration / 1000.0
+	sample_rate = 44100
+	total_samples = int(total_duration_sec * sample_rate)
+	t = np.arange(total_samples) / sample_rate
+	if ptm:
+		segment_times = np.linspace(0, total_duration_sec, len(frequencies), endpoint=False)
+		freq_array = np.interp(t, segment_times, frequencies, left=frequencies[0], right=frequencies[-1])
+	else:
+		segment_samples = total_samples // len(frequencies)
+		freq_array = np.zeros(total_samples)
+		for i, freq in enumerate(frequencies):
+			start = i * segment_samples
+			end = start + segment_samples
+			if i == len(frequencies) - 1:
+				end = total_samples
+			freq_array[start:end] = freq
+	phase = 2 * np.pi * np.cumsum(freq_array / sample_rate)
+	audio_signal = np.sin(phase)
+	audio_signal *= vol
+	fade_samples = int(0.7 * sample_rate)  # 150 ms in numero di campioni
+	if fade_samples * 2 > total_samples:
+		fade_samples = total_samples // 2  # Assicura che il fade non sia più lungo del segnale
+	fade_in = np.linspace(0, 1, fade_samples)
+	fade_out = np.linspace(1, 0, fade_samples)
+	audio_signal[:fade_samples] *= fade_in
+	audio_signal[-fade_samples:] *= fade_out
+	pan = np.linspace(-1.0, 1.0, total_samples)
+	left = audio_signal * ((1 - pan) / 2)
+	right = audio_signal * ((1 + pan) / 2)
+	audio = np.column_stack((left, right))
+	sd.play(audio, samplerate=sample_rate)
+	sd.wait()
 	return
-
-def sonify2(data=[], totalduration=10.0, soundlowerlimit=130.0, soundupperlimit=4186.0, vol=.35, ptm=True):
-	# Sonify2 a serie of data, V4.0, march 19th, 2024
-	# list data= values to sonify
-	# float totalduration= duration of whole sonification in seconds
-	# it plays sound and returns nothing
-	# default lower and upper sound limit are C3 and C8 notes
-	# float vol is amplitude
-	# bool ptm = True determines if portamento
-	totalduration=float(totalduration)
-	datalength = len(data)
-	if datalength < 2:
-		print("Data serie too short.")
-		return
-	from pyo import Server, SigTo, Sine, Pan
-	from threading import Thread
-	from time import sleep
-	s=Server(verbosity=1).boot()
-	s.start()
-	def Sonip():
-		# It creates sound with portamento
-		singlesoundduration=totalduration/datalength
-		soundrange=soundupperlimit - soundlowerlimit
-		datarange = max(data) - min(data)
-		startsound=soundlowerlimit + soundrange/2
-		slide=SigTo(init=data[0], value=data[0], time=singlesoundduration)
-		sound=Sine(freq=slide, mul=vol)
-		stereo=SigTo(init=0.0, value=1.0, time=totalduration)
-		p=Pan(sound, pan=stereo).out()
-		for j in data:
-			x = (j-min(data)) * 100/datarange
-			y = x/100*soundrange
-			slide.value=soundlowerlimit + y
-			sleep(singlesoundduration)
-		sleep(singlesoundduration)
-		s.stop()
-		s.shutdown()
-		return
-	def Sonis():
-		# It creates sound sliced, no portamento
-		singlesoundduration=totalduration/datalength
-		soundrange=soundupperlimit - soundlowerlimit
-		datarange = max(data) - min(data)
-		startsound=soundlowerlimit + soundrange/2
-		slide=data[0]
-		sound=Sine(freq=slide, mul=vol)
-		stereo=SigTo(init=0.0, value=1.0, time=totalduration)
-		p=Pan(sound, pan=stereo).out()
-		for j in data:
-			x = (j-min(data)) * 100/datarange
-			y = x/100*soundrange
-			sound.freq=soundlowerlimit + y
-			sleep(singlesoundduration)
-		sleep(singlesoundduration)
-		s.stop()
-		s.shutdown()
-		return
-	if ptm: t1=Thread(target=Sonip)
-	else: t1=Thread(target=Sonis)
-	t1.start()
-	return
-
+	
 def dgt(prompt="", kind="s", imin=-999999999, imax=999999999, fmin=-999999999.9, fmax=999999999.9, smin=0, smax=256, pwd=False, default=None):
 	'''Versione 1.9 di lunedì 17 aprile 2023
 	Potenzia la funzione input implementando controlli di sicurezza.
