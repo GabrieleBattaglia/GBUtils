009915e51e75a4b8521214ec0faf68cf386bb2e4
GabrieleBattaglia <iz4apu@libero.it>
2025-07-09 14:32:11 +0200

    new file:   setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='GBUtils',
+    version='53',
+    packages=find_packages(),
+    author='Gabriele Battaglia',
+    author_email='tuo.iz4apu@libero.it',
+    description='Pacchetto di utilities varie per i miei software',
+    url='https://github.com/GabrieleBattaglia/GBUtils',
+)
